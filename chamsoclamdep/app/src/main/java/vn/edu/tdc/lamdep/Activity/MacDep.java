d013d5c61c4e231a2025feac9a90624fb73af491
=namTT6 <=nguyenvannamcd16tt6thdc@gmail.com>
2019-04-09 16:50:36 +0700

    commit

--- chamsoclamdep/app/src/main/java/vn/edu/tdc/lamdep/Activity/MacDep.java
+++ chamsoclamdep/app/src/main/java/vn/edu/tdc/lamdep/Activity/MacDep.java
@@ -1,23 +1,15 @@
 package vn.edu.tdc.lamdep.Activity;
-
-import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ViewFlipper;
-
-import java.util.ArrayList;
-
-import vn.edu.tdc.lamdep.Adapter.MacDep_Adapter;
 import vn.edu.tdc.lamdep.R;
-import vn.edu.tdc.lamdep.model.Macdep_model;
+
 
 public class MacDep extends Fragment {
     // Các thuộc tính
