47ace0bb6d051a5d30f20451db370544e3cd84a5
=namTT6 <=nguyenvannamcd16tt6thdc@gmail.com>
2019-04-09 15:56:14 +0700

    commit

---
+++ chamsoclamdep/app/src/main/java/vn/edu/tdc/lamdep/SplashScreen/SplashScreen/manHinhChao.java
@@ -0,0 +1,31 @@
+package vn.edu.tdc.lamdep.SplashScreen.SplashScreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.edu.tdc.lamdep.Activity.MainActivity;
+import vn.edu.tdc.lamdep.R;
+
+public class manHinhChao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chao);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent intent = new Intent(manHinhChao.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        thread.start();
+    }
+}
